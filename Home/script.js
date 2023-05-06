041fbe10b4538998d2460e00c163b412e5c9dfd0
DTom59 <114876153+DTom59@users.noreply.github.com>
2023-05-07 01:06:06 +0200

    Add files via upload

--- Home/script.js
+++ Home/script.js
@@ -1,16 +1,17 @@
 const hamburger = document.querySelector(".hamburger");
 const navlinksContainer = document.querySelector(".navlinks-container");
+const main = document.querySelector("main");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("open");
   navlinksContainer.classList.toggle("open");
+  main.classList.toggle("open");
 });
 
-new ResizeObserver(entries => {
-  if(entries[0].contentRect.width <= 850) {
-    navlinksContainer.style.transition = "transform 0.3s ease-out"
-  } 
-  else {
-    navlinksContainer.style.transition = "none"
+new ResizeObserver((entries) => {
+  if (entries[0].contentRect.width <= 850) {
+    navlinksContainer.style.transition = "transform 0.2s 0.15s ease-out";
+  } else {
+    navlinksContainer.style.transition = "none";
   }
 }).observe(document.body);

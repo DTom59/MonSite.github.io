1671ab68569ee70b704bd7bd8863f8fe6db353b7
DTom59 <114876153+DTom59@users.noreply.github.com>
2023-05-10 13:33:02 +0200

    Add files via upload

--- script2.js
+++ script2.js
@@ -18,17 +18,20 @@ function removeClass(event) {
   addonA.classList.remove("fa-bounce");
 }
 
-const nlksA = document.querySelectorAll(".navlinks-container a");
+const nlA = document.querySelectorAll(".navlinks-container a");
 
-nlksA.forEach(function (nvlksA) {
-  nvlksA.addEventListener("mouseenter", addClass);
-  nvlksA.addEventListener("mouseleave", removeClass);
+nlA.forEach(function (nlAset) {
+  nlAset.addEventListener("mouseenter", addClass);
+  nlAset.addEventListener("mouseleave", removeClass);
 });
 
 new ResizeObserver((entries) => {
-  if (entries[0].contentRect.width <= 850) {
+  console.log(entries);
+  if (entries[0].contentRect.width <= 941) {
     navlinksContainer.style.transition = "transform 0.2s 0.15s ease-out";
   } else {
     navlinksContainer.style.transition = "none";
   }
 }).observe(document.body);
+
+console.log(entries);

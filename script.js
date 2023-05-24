e89d6378db2acb1823fd75a871f53f824e5fcdaa
DTom59 <114876153+DTom59@users.noreply.github.com>
2023-05-24 12:04:52 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -25,6 +25,8 @@ nlA.forEach(function (nlAset) {
   nlAset.addEventListener("mouseleave", removeClass);
 });
 
+// Correction of transition on the Resize page
+
 new ResizeObserver((entries) => {
   if (entries[0].contentRect.width <= 941) {
     navlinksContainer.style.transition = "transform 0.2s 0.15s ease-out";
@@ -33,40 +35,63 @@ new ResizeObserver((entries) => {
   }
 }).observe(document.body);
 
+//API OpenWeather
+
 const weatherAPI = "b8fe848a27a98090e0f21d87f4d69fe4";
 
-let url =
-  "https://api.openweathermap.org/data/2.5/weather?q=Halluin&appid=" +
-  weatherAPI +
-  "&units=metric&lang=fr";
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+} else {
+  console.log(
+    "La géolocalisation n'est pas prise en charge par votre navigateur."
+  );
+}
 
-fetch(url).then((response) =>
-  response.json().then((data) => {
-    console.log(data);
-    document.querySelector(".city div").innerHTML = data.name;
+function successCallback(position) {
+  lat = position.coords.latitude;
+  long = position.coords.longitude;
+  let url =
+    "https://api.openweathermap.org/data/2.5/weather?lat=" +
+    lat +
+    "&lon=" +
+    long +
+    "&appid=" +
+    weatherAPI +
+    "&units=metric&lang=fr";
+  fetch(url).then((response) =>
+    response.json().then((data) => {
+      console.log(data);
+      document.querySelector(".city div").innerHTML = data.name;
 
-    document.querySelector(".temp div").innerHTML =
-      Math.trunc(data.main.temp) + "°C";
+      document.querySelector(".temp div").innerHTML =
+        Math.trunc(data.main.temp) + "°C";
 
-    document.querySelector(".wind div").innerHTML =
-      Math.trunc(data.wind.speed) + " km/h";
-    ddeg = data.wind.deg;
-    const arrowWind = document.querySelector(".arrow-wind");
-    arrowWind.style.transform = "rotate(" + ddeg + "deg)";
+      document.querySelector(".wind div").innerHTML =
+        Math.trunc(data.wind.speed) + " km/h";
+      winddeg = data.wind.deg;
+      const arrowWind = document.querySelector(".arrow-wind");
+      arrowWind.style.transform = "rotate(" + winddeg + "deg)";
 
-    document.querySelector(".humidity div").innerHTML =
-      data.main.humidity + "%";
+      document.querySelector(".humidity div").innerHTML =
+        data.main.humidity + "%";
 
-    weatherDescri = data.weather[0].description;
-    const skyContainer = document.querySelector(".sky .content-div");
-    skyContainer.innerHTML = weatherDescri;
-    const skyI = document.querySelector(".sky-icon");
-    if (weatherDescri === "pluie") {
-      skyI.classList.add("fa-solid", "fa-cloud-rain");
-    } else if (weatherDescri === "ensoleillé") {
-      skyI.classList.add("fa-solid", "fa-sun");
-    } else {
-      skyI.classList.add("fa-solid", "fa-cloud");
-    }
-  })
-);
+      weatherDescri = data.weather[0].description;
+      const skyContainer = document.querySelector(".sky .content-div");
+      skyContainer.innerHTML = weatherDescri;
+      const skyIcon = document.querySelector(".sky-icon");
+      if (weatherDescri === "pluie") {
+        skyIcon.classList.add("fa-solid", "fa-cloud-rain");
+      } else if (weatherDescri === "ensoleillé") {
+        skyIcon.classList.add("fa-solid", "fa-sun");
+      } else {
+        skyIcon.classList.add("fa-solid", "fa-cloud");
+      }
+    })
+  );
+}
+
+function errorCallback(error) {
+  console.log(
+    "Erreur lors de la récupération de la position : " + error.message
+  );
+}
